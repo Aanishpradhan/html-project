115c0a647c57f2dbe4d32d10c835a851211891d9
aryan <anishpradhan.96@gamil.com>
2022-07-22 16:15:14 +0530

    first project html

---
+++ office/login.js
@@ -0,0 +1,13 @@
+function validate (){
+    var username=document.getElementById("username").value;
+    var username=document.getElementById("username").value;
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=='first name'&& password=='user'){
+        alert("login succesfully");
+        return false;
+    }else{
+        alert("login succesfully");
+    }
+
+}
